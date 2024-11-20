423b76115d604d2a4f4d55c973a9a9881cfe708f
Ben Warren <warrenbhw@gmail.com>
2024-11-20 15:12:48 -0800

    use tool call package in chat

--- example/app/server/chat.ts
+++ example/app/server/chat.ts
@@ -1,7 +1,8 @@
-import { createServerFn } from '@tanstack/start'
-import OpenAI from 'openai';
-import { z } from 'zod';
-import { openAiMessagesSchema } from '../lib/types';
+import { createServerFn } from "@tanstack/start";
+import OpenAI from "openai";
+import { z } from "zod";
+import { openAiMessagesSchema } from "../lib/types";
+import { callTools, discoverTools, toOpenAIFormat } from "@snowpilot/tool-cool";
 
 async function makeOpenAiClient(): Promise<OpenAI> {
 	return new OpenAI({
@@ -10,24 +11,34 @@ async function makeOpenAiClient(): Promise<OpenAI> {
 }
 
 export const toolChat = createServerFn()
-	.validator(z.object({
-		messages: openAiMessagesSchema,
-	}))
-.handler(async (ctx) => {
-	const client = await makeOpenAiClient();
+	.validator(
+		z.object({
+			messages: openAiMessagesSchema,
+		}),
+	)
+	.handler(async (ctx) => {
+		const client = await makeOpenAiClient();
 
-	try {
-		const chatCompletion = await client.chat.completions.create({
-			messages: ctx.data.messages,
-			model: "gpt-4o-mini",
-			// tools,
-			n: 1,
-		});
+		const hnTools = await discoverTools(["hacker-news"]);
 
-		return chatCompletion.choices[0].message;
-	} catch (error) {
-		//TODO: handle error
-		console.log(error);
-	}
+		try {
+			const chatCompletion = await client.chat.completions.create({
+				messages: ctx.data.messages,
+				model: "gpt-4o-mini",
+				tools: toOpenAIFormat(...hnTools),
+				tool_choice: "auto",
+				n: 1,
+			});
 
-})
+			const toolCalls = chatCompletion.choices[0].message.tool_calls;
+			if (toolCalls) {
+				const results = await callTools(hnTools, toolCalls);
+				console.log("tool results", results);
+			}
+
+			return chatCompletion.choices[0].message;
+		} catch (error) {
+			//TODO: handle error
+			console.log(error);
+		}
+	});
