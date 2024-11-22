df5d710c3aa1dce665592bc0a1a107e7fc893a5c
Ben Warren <warrenbhw@gmail.com>
2024-11-22 04:35:21 -0800

    improve types

--- example/app/lib/types.ts
+++ example/app/lib/types.ts
@@ -1,33 +1,44 @@
 import { z } from "zod";
 
-// TODO: should just get this from openai
 export const toolCallSchema = z.object({
+	id: z.string(),
 	function: z.object({
 		name: z.string(),
 		arguments: z.string(),
 	}),
 	type: z.literal("function"),
-	id: z.string(),
 });
 
 export type ToolCall = z.infer<typeof toolCallSchema>;
 
-export const openAiMessageSchema = z.object({
-	role: z.enum(["system", "user", "assistant", "tool"]),
-	content: z.string().nullable().optional(),
-	refusal: z.string().nullable().optional(),
-	audio: z
-		.object({
-			id: z.string(),
-			data: z.string(),
-			expires_at: z.number(),
-			transcript: z.string(),
-		})
-		.nullable()
-		.optional(),
-	tool_calls: z.array(toolCallSchema).nullable().optional(),
-	tool_call_id: z.string().nullable().optional(),
-});
+export const openAiMessageSchema = z.discriminatedUnion("role", [
+	z.object({
+		role: z.literal("tool"),
+		content: z.string(),
+		tool_call_id: z.string(),
+	}),
+	z.object({
+		role: z.enum(["system", "user"]),
+		content: z.string(),
+		name: z.string().optional(),
+	}),
+	z.object({
+		role: z.literal("assistant"),
+		content: z.string().nullable().optional(),
+		refusal: z.string().nullable().optional(),
+		audio: z
+			.object({
+				id: z.string(),
+				data: z.string(),
+				expires_at: z.number(),
+				transcript: z.string(),
+			})
+			.nullable()
+			.optional(),
+		tool_calls: z.array(toolCallSchema).optional(),
+		name: z.string().optional(),
+	}),
+]);
 
 export type MessageType = z.infer<typeof openAiMessageSchema>;
 
--- example/app/server/chat.ts
+++ example/app/server/chat.ts
@@ -6,7 +6,6 @@ import {
 } from "@snowpilot/toolcool";
 import { createServerFn } from "@tanstack/start";
 import OpenAI from "openai";
-import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 import { z } from "zod";
 import { openAiMessagesSchema, toolCallSchema } from "../lib/types";
 
@@ -21,17 +20,10 @@ export const toolChat = createServerFn({
 			messages: openAiMessagesSchema,
 		}).parse,
 	)
-	.handler(async ({ data }) => {
-		const messagesToSend = data.messages.map((msg) => ({
-			role: msg.role,
-			content: msg.content,
-			tool_calls: msg.tool_calls,
-			tool_call_id: msg.tool_call_id,
-		})) as ChatCompletionMessageParam[];
-
+	.handler(async ({ data: { messages } }) => {
 		try {
 			const chatCompletion = await openai.chat.completions.create({
-				messages: messagesToSend,
+				messages,
 				model: "gpt-4o-mini",
 				tools: toOpenAIFormat(tools),
 				tool_choice: "auto",
@@ -57,11 +49,11 @@ export const callTool = createServerFn({
 			tool_call: toolCallSchema,
 		}).parse,
 	)
-	.handler(async ({ data }) => {
-		const result = await callTools(tools, [data.tool_call]);
+	.handler(async ({ data: { tool_call } }) => {
+		const result = await callTools(tools, [tool_call]);
 		return {
 			role: "tool",
 			content: JSON.stringify(result),
-			tool_call_id: data.tool_call.id,
+			tool_call_id: tool_call.id,
 		};
 	});
