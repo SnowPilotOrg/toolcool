82d0154af35362cabd4bec664c73b0def1b85b86
Dom Crosby <domcrosby1@gmail.com>
2024-11-21 16:08:34 -0800

    Add: shadcn

--- example/app/components/recommendations.tsx
+++ example/app/components/recommendations.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@nextui-org/react";
+import { Avatar, AvatarGroup, Button } from "@nextui-org/react";
+import { BrandIcon } from "./brand-icon";
 
 export const Recommendations = ({
 	handleSend,
@@ -6,31 +7,43 @@ export const Recommendations = ({
 	<div className="flex flex-wrap gap-2">
 		{[
 			{
+				brand: ["hacker-news"],
 				text: "Latest HN News",
 				prompt: "Explain the latest news from Hacker News",
 			},
 			{
+				brand: ["product-hunt", "hacker-news"],
 				text: "Trending Topics",
-				prompt: "What are the top trending topics?",
+				prompt: "What are the top trending topics on hacker news and product hunt",
 			},
 			{
+				brand: ["hacker-news"],
 				text: "Interesting Discussions",
-				prompt: "Summarize the most interesting discussions",
+				prompt: "Summarize the most interesting discussions on hacker news",
 			},
 			{
+				brand: ["product-hunt"],
 				text: "Top Products",
 				prompt:
-					"What are the most upvoted products on Product Hunt and Hacker News today?",
+					"What are the most upvoted products on Product Hunt today?",
 			},
-		].map(({ text, prompt }) => (
+		].map(({ brand, text, prompt }) => (
 			<Button
 				key={text}
-				size="sm"
+				size="md"
 				variant="flat"
 				onClick={() => {
 					handleSend(prompt);
 				}}
+				className="flex items-center gap-2"
 			>
+				<div className="flex gap-1">
+					{brand.map((b) => (
+						
+							<BrandIcon brand={b as "hacker-news" | "product-hunt"} />
+				
+					))}
+				</div>
 				{text}
 			</Button>
 		))}
